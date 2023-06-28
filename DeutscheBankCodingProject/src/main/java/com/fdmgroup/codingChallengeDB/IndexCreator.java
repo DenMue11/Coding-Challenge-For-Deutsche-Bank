a8e9b926ae47d267f087ab0c2e44d50479e77bda
DenMue11 <101323522+DenMue11@users.noreply.github.com>
2023-06-28 09:26:10 +0200

    Add files via upload

--- DeutscheBankCodingProject/src/main/java/com/fdmgroup/codingChallengeDB/IndexCreator.java
+++ DeutscheBankCodingProject/src/main/java/com/fdmgroup/codingChallengeDB/IndexCreator.java
@@ -3,33 +3,72 @@ package com.fdmgroup.codingChallengeDB;
 import tech.tablesaw.api.Table;
 
 public class IndexCreator {
+	
+	private double weightTRX = 0.2;
+	private double weightNGL= 0.4;
+	private double weightMEGA= 0.3;
+	private double weightABC= 0.1;
 
 	public double[] createIndexDoubleArray(int columnIndex, Table table1, Table table2, Table table3, Table table4) {
 		double[] indexDoubleArray = new double[7];
 		for (int i = 0; i <= 6; i++) {
 			if (i == 1) {
-				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * 0.1)
-						+ (table2.doubleColumn(columnIndex).getDouble(i) * 0.3)
-						+ (table3.doubleColumn(columnIndex).getDouble(i - 1) * 0.2)
-						+ (table4.doubleColumn(columnIndex).getDouble(i - 1) * 0.4);
+				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * weightABC)
+						+ (table2.doubleColumn(columnIndex).getDouble(i) * weightMEGA)
+						+ (table3.doubleColumn(columnIndex).getDouble(i - 1) * weightTRX)
+						+ (table4.doubleColumn(columnIndex).getDouble(i - 1) * weightNGL);
 			} else if (i > 1 && i < 6) {
-				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * 0.1)
-						+ (table2.doubleColumn(columnIndex).getDouble(i) * 0.3)
-						+ (table3.doubleColumn(columnIndex).getDouble(i) * 0.2)
-						+ (table4.doubleColumn(columnIndex).getDouble(i) * 0.4);
+				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * weightABC)
+						+ (table2.doubleColumn(columnIndex).getDouble(i) * weightMEGA)
+						+ (table3.doubleColumn(columnIndex).getDouble(i) * weightTRX)
+						+ (table4.doubleColumn(columnIndex).getDouble(i) * weightNGL);
 			} else if (i == 6) {
-				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * 0.1)
-						+ (table2.doubleColumn(columnIndex).getDouble(i) * 0.3)
-						+ (table3.doubleColumn(columnIndex).getDouble(i - 1) * 0.2)
-						+ (table4.doubleColumn(columnIndex).getDouble(i) * 0.4);
+				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * weightABC)
+						+ (table2.doubleColumn(columnIndex).getDouble(i) * weightMEGA)
+						+ (table3.doubleColumn(columnIndex).getDouble(i - 1) * weightTRX)
+						+ (table4.doubleColumn(columnIndex).getDouble(i) * weightNGL);
 			} else {
-				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * 0.1)
-						+ (table2.doubleColumn(columnIndex).getDouble(i) * 0.3)
-						+ (table3.doubleColumn(columnIndex).getDouble(i) * 0.2)
-						+ (table4.doubleColumn(columnIndex).getDouble(i) * 0.4);
+				indexDoubleArray[i] = (table1.doubleColumn(columnIndex).getDouble(i) * weightABC)
+						+ (table2.doubleColumn(columnIndex).getDouble(i) * weightMEGA)
+						+ (table3.doubleColumn(columnIndex).getDouble(i) * weightTRX)
+						+ (table4.doubleColumn(columnIndex).getDouble(i) * weightNGL);
 			}
 		}
 		return indexDoubleArray;
 	}
+
+	public double getWeightTRX() {
+		return weightTRX;
+	}
+
+	public void setWeightTRX(double weightTRX) {
+		this.weightTRX = weightTRX;
+	}
+
+	public double getWeightNGL() {
+		return weightNGL;
+	}
+
+	public void setWeightNGL(double weightNGL) {
+		this.weightNGL = weightNGL;
+	}
+
+	public double getWeightMEGA() {
+		return weightMEGA;
+	}
+
+	public void setWeightMEGA(double weightMEGA) {
+		this.weightMEGA = weightMEGA;
+	}
+
+	public double getWeightABC() {
+		return weightABC;
+	}
+
+	public void setWeightABC(double weightABC) {
+		this.weightABC = weightABC;
+	}
+	
+	
 
 }
